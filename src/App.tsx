412f2f27e052789bb7907f6b4da9c50e3d3da952
刘雷 <wb-liulei1@cai-inc.com>
2023-06-10 23:29:11 +0800

    feat: test1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Tip2 from './components/Tip2';
 function App() {
   return (
     <>
-      <span className={styles['font-color']}>通过less module 修改颜色11111</span>
+      <span className={styles['font-color']}>通过less module 修改颜色111112222</span>
       <Test1 />
       <Tip2 />
       <Button type='primary'>Button1</Button>
